158327d7b68ceec48ab1f3258ae147db4d4024f8
chuhx <chuhx@yonyou.com>
2019-11-16 16:20:57 +0800

    动画实现

--- src/XiaojiejieItem.js
+++ src/XiaojiejieItem.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class XiaojiejieItem extends Component {
+    // 生命周期 start----------------------------
+    componentWillReceiveProps () { 
+        // 组件第一次存在于dom中 函数不会执行
+        // 已经存在dom中  函数才会执行
+        console.log('我要接受参数了')
+    }
+    componentWillUnmount () {
+        console.log('Unmount')
+    }
+    // 生命周期 end------------------------------
     render() {
+        console.log("我再渲染")
         return (
             <div>
                 <li
